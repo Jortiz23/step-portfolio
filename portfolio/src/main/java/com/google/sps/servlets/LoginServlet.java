2746ab08c7852ad3adf1906e72726dc9703d75d7
Jortiz23 <jortiz23@verizon.net>
2020-06-24 02:08:06 +0000

    Implement a simple login feature

---
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -0,0 +1,48 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        UserService userService = UserServiceFactory.getUserService();
+        if (userService.isUserLoggedIn()) {
+            String logoutUrl = userService.createLogoutURL("/index.html");
+            String logoutDetails = "<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>" +
+                                    "<form action = \"/login\" method = \"post\">" +
+                                    "<button type = \"submit\">Go Back</button>" +
+                                    "</form>";
+            response.getWriter().println(logoutDetails);
+        } else {
+            String loginUrl = userService.createLoginURL("/index.html");
+            String loginDetails = "<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>" +
+                                    "<form action = \"/login\" method = \"post\">" +
+                                    "<button type = \"submit\">Go Back</button>" +
+                                    "</form>";
+            response.getWriter().println(loginDetails);
+        }
+    }
+}
